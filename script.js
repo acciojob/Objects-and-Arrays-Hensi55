113d5254b9569d18b6789a1978664fab77986b15
Hensi55 <120088226+Hensi55@users.noreply.github.com>
2023-04-20 23:56:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ const person = {
   name: "John Doe",
   age: 80,
 };
+const team = players;
+const team1 = [...players];
+const cap1 = {...person};
 
+export {players,person,team,team1,cap1};
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
 window.players = players;
